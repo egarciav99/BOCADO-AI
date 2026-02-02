7378f2836ad19482ed4960a2a805abecd5b07c0e
Elier <236448969+egarciav99@users.noreply.github.com>
2026-02-02 19:19:39 +0000

    Colores agregados

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,25 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./api/**/*.{js,ts,jsx,tsx}",
+   
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/api/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bocado-green': '#316559',
+        'bocado-green-light': '#6E9277',
+        'bocado-gray': '#9DB3C1',
+        'bocado-dark-gray': '#374F59',
+        'bocado-dark-green': '#2C4F40',
+        'bocado-background': '#F9F7F2',
+        'bocado-text': '#252423',
+      },
+      fontFamily: {
+        sans: ['Verdana', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
